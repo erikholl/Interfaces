679de12d3544e983092ff4f19ca915f5c3141b9d
erikholl <erik.hollanders@student.intecbrussel.be>
2021-11-19 20:11:52 +0100

    NegativeSizeException added + some minor changes

--- src/be/intecbrussel/geometry/DrawingContext.java
+++ src/be/intecbrussel/geometry/DrawingContext.java
@@ -1,12 +1,9 @@
 package be.intecbrussel.geometry;
 
-import javax.swing.*;
-import java.awt.*;
-
 public interface DrawingContext {
 
     void draw(Rectangle rectangle);
     void draw(Circle circle);
     void draw(Triangle triangle);
-//    void draw(Graphics2D g2d);
+
 }
